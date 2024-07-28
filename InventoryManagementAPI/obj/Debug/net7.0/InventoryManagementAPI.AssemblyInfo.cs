cf4c1fcefc5f5a1e702cacd5c009d327b4af9db8
collo-n <collinsnyachoti@gmail.com>
2024-07-28 12:16:10 +0300

    Angular update

--- InventoryManagementAPI/obj/Debug/net7.0/InventoryManagementAPI.AssemblyInfo.cs
+++ InventoryManagementAPI/obj/Debug/net7.0/InventoryManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7bfc13937921ee6594b0d173581250821cb973")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
